25ae59d8e2563e2c5699da04d8e732904be8a2f2
photomanai <photom4n.41@gmail.com>
2024-11-28 00:21:57 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SlideBar from "./components/SlideBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <SlideBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="main_navbar">
+      <h1 className="navbar_header">Adil</h1>
+      <ul className="mini_navbar">
+        <li className="mini_navbar_paragraf">Home</li>
+        <li className="mini_navbar_paragraf">About</li>
+        <li className="mini_navbar_paragraf">Projects</li>
+        <li className="mini_navbar_paragraf">Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
