7b3d93d5a400896d04a91b42adf41c6ec1fb7cc7
photomanai <photom4n.41@gmail.com>
2024-12-01 13:42:14 +0400

    get cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import homeRight from "../assets/home-right.png";
 const Home = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.download = "ADIL_CV.pdf"; // File name for download
-    link.href = "/RufanCV.pdf"; // File in public folder, no import required
+    link.download = "AdilAbdulkerim_CV.pdf"; // İndirilecek dosya adı
+    link.href = "/Adils-portfolio/ADIL_CV.pdf"; // public klasöründen erişim yolu
     link.click();
   };
 
@@ -17,10 +17,14 @@ const Home = () => {
           <h1 className="home_header_paragraf text">I AM ADIL ABDULKERIM</h1>
         </div>
         <div className="home_btn_container">
-          <button className="home_btn">Hire Me</button>
+          <a href="#contact">
+            <button className="home_btn">Hire Me</button>
+          </a>
+          {/* <a href="../src/ADIL_CV.pdf" download> */}
           <button className="home_btn" onClick={handleDownload}>
             Get CV
           </button>
+          {/* </a> */}
         </div>
       </div>
       <div className="home_img_container">
